bc68b514f5c032b9e13c2bada138ab6752efadf7
rpelletierrefocus <47399964+rpelletierrefocus@users.noreply.github.com>
2019-10-25 09:08:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 chmod +x usr/local/bin/*
 cp -r usr/local/bin/* /usr/local/bin/
 cp -r service_* /etc/zabbix/
-
+cp -r userparameter_systemd_services.conf /etc/zabbix/zabbix_agentd.d/
+systemctl restart zabbix-agent
