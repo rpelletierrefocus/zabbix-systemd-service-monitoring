c2637370a515c6fdcbe4da4c961b59bcc68f1788
rpelletierrefocus <47399964+rpelletierrefocus@users.noreply.github.com>
2019-10-25 09:10:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cp -r usr/local/bin/* /usr/local/bin/
 cp -r service_* /etc/zabbix/
 cp -r userparameter_systemd_services.conf /etc/zabbix/zabbix_agentd.d/
 systemctl restart zabbix-agent
+zabbix_agentd -t "systemd.service.discovery"
